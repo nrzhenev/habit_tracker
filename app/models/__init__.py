71eec615eebee95e25018cb9900419857fc12cf0
nrzhenev <nikita.rzhenev@gmail.com>
2026-05-31 05:28:56 +0400

    refactor: remove Habit and Completion

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,4 @@
 from app.models.base import Base
-from app.models.completion import Completion
-from app.models.habit import Habit
 from app.models.user import User
 
-__all__ = ["Base", "User", "Habit", "Completion"]
+__all__ = ["Base", "User"]
